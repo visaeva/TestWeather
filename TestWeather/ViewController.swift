454dfdd8ef4af0fc6d5735cb993056a6ef028d70
visaeva <visaeva0302@gmail.com>
2025-05-13 19:52:32 +0300

    settings commit

--- TestWeather/ViewController.swift
+++ TestWeather/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+	private lazy var backgroundImage: UIImageView = {
+		let image = UIImageView()
+		image.image = UIImage(named: "background")
+		image.contentMode = .scaleAspectFill
+		image.translatesAutoresizingMaskIntoConstraints = false
+		return image
+	}()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		setupUI()
 	}
-
-
+	
+	private func setupUI() {
+		view.addSubview(backgroundImage)
+	}
+	
+	private func setupConstraints() {
+		NSLayoutConstraint.activate([
+			backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+			backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+		])
+	}
 }
 
